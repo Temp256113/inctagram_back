fc1936368c71984fe29dc227e7abb9f3c5ae47b4
Alexandr <smolnikov.456@mail.ru>
2024-06-16 23:06:16 +0500

    remove unnecessary code

--- libs/repositories/src/repositories.module.ts
+++ libs/repositories/src/repositories.module.ts
@@ -3,25 +3,17 @@ import { PrismaService } from '@libs/repositories/prisma.service';
 import { UserQueryRepository } from '@libs/repositories/query-repos/user.queryRepository';
 import { UserRepository } from '@libs/repositories/repos/user.repository';
 import { UserPostsQueryRepository } from '@libs/repositories/query-repos/userPosts.queryRepository';
-import { FileResourceQueryRepository } from '@libs/repositories/query-repos/fileResource.queryRepository';
 import { UserProfileQueryRepository } from '@libs/repositories/query-repos/userProfile.queryRepository';
-import { FileResourceRepository } from '@libs/repositories/repos/fileResource.repository';
 import { UserPostsRepository } from '@libs/repositories/repos/userPosts.repository';
 import { UserProfileRepository } from '@libs/repositories/repos/userProfile.repository';
 
 const queryRepos = [
-  FileResourceQueryRepository,
   UserQueryRepository,
   UserPostsQueryRepository,
   UserProfileQueryRepository,
 ];
 
-const repos = [
-  FileResourceRepository,
-  UserRepository,
-  UserPostsRepository,
-  UserProfileRepository,
-];
+const repos = [UserRepository, UserPostsRepository, UserProfileRepository];
 
 const providers = [PrismaService, ...queryRepos, ...repos];
 
