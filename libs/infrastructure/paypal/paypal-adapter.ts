8e01f2b2399911213f7c8505d3b5a50eec40b7a1
Alexey Borisenko <bgr666@gmail.com>
2025-02-04 00:13:24 +0300

    fix: test commit

--- libs/infrastructure/paypal/paypal-adapter.ts
+++ libs/infrastructure/paypal/paypal-adapter.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import axios, { Axios } from 'axios';
 import * as PaymentContentMicroserviceTypes from '@libs/common-types/payment/microservice';
 import { startWith } from 'rxjs';
-import * as dateFns from 'date-fns';
 
 @Injectable()
 export class PaypalAdapter {
@@ -291,7 +290,6 @@ export class PaypalAdapter {
         });
       } catch (err) {
         console.log('createPayment ERR: ');
-        // console.log(err);
         return;
       }
 
